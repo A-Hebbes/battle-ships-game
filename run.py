1cd110f186442baa27c05de036176f25baa09dd7
Alex Hebbes <aeh7@live.co.uk>
2024-04-14 07:46:49 +0000

    feat: sort formatting

--- run.py
+++ run.py
@@ -1,32 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-
-#https://www.youtube.com/watch?v=tF1WRCrd_HQ Used this video for guidance
-
-"""
-Issues to sort. 
-
-
-
-
-"""
-
-#Imports 
+# Imports
 
 from random import randint, choice
 
-#Settings for difficulty and game boards
+# Settings for difficulty and game boards
 
-letters_to_numbers = {'A' : 0, 'B' : 1, 'C' : 2, 'D' : 3, 'E' : 4, 'F' : 5, 'G' : 6, 'H' : 7, 'I' : 8, 'J' : 9}
-ship_sizes = {
-    'easy': [3, 3, 2, 2, 1, 1],  
-    'hard': [1, 1, 1, 1, 1, 1] 
+letters_to_numbers = {
+    'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4,
+    'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9
 }
 
-#Functions for user choice of game type 
+ship_sizes = {
+    'easy': [3, 3, 2, 2, 1, 1],
+    'hard': [1, 1, 1, 1, 1, 1]
+}
 
-def choose_board_size(): 
+# Functions for user choice of game type
+
+
+def choose_board_size():
     print("Welcome to Battleships")
     print("You will first select a board size and difficulty")
     print("Then you will have the option to read the rules")
@@ -36,18 +27,19 @@ def choose_board_size():
     print("1. Small 5x5")
     print("2. Medium 8x8")
     print("3. Large 10x10")
-    choice = input ("Enter choice (1-3): ")
+    choice = input("Enter choice (1-3): ")
     if choice == '1':
-        return 5 
+        return 5
     elif choice == '2':
         return 8
     elif choice == '3':
         return 10
     else:
-        print("------------------------------------------------------") 
-        print ("Your Choice Wasn't Valid. Defauting to a small board.")
-        print("------------------------------------------------------") 
-        return 5 
+        print("------------------------------------------------------")
+        print("Your Choice Wasn't Valid. Defauting to a small board.")
+        print("------------------------------------------------------")
+        return 5
+
 
 def choose_difficulty():
     print("^^^^^^^^^^^^^^^^^^")
@@ -63,7 +55,9 @@ def choose_difficulty():
         print("Invalid choice. Defaulting to Easy.")
         return 'easy'
 
-#Function for Game Rules 
+# Function for Game Rules
+
+
 def display_rules():
     print("^^^^^^^^^^^^^^^^^^^^^^^")
     print("Welcome to Battleships!")
@@ -73,32 +67,33 @@ def display_rules():
     print("- You will choose a location on the opponent's board to target.")
     print("- If your shot hits a battleship, it will be marked with 'X'.")
     print("- If your shot misses, it will be marked with '-'.")
-    print("- If you make a successful hit, you will activate a sonar to scan adjacent squares")
-    print("- The game ends when you sink all of your opponent's battleships or run out of turns.")
+    print("- A successful hit activates sonar scan of adjacent squares")
+    print("- Game ends if all opponent's ships are sunk or no more turns.")
     print("\nLet's get started!\n")
 
-#Board Creation and Display 
-#--------------------------
+# Board Creation and Display
+# --------------------------
 
-#Function for board display
+# Function for board display
+
 
 def show_board(board, board_size):
     row_number = 1
     if board_size == 10:
-        print ('   A B C D E F G H I J')
-        print ('   -------------------')
+        print('   A B C D E F G H I J')
+        print('   -------------------')
     elif board_size == 8:
-        print ('   A B C D E F G H')
-        print ('   ---------------')
+        print('   A B C D E F G H')
+        print('   ---------------')
     else:
-        print ('   A B C D E')
-        print ('   ---------')
-    for row in board: 
+        print('   A B C D E')
+        print('   ---------')
+    for row in board:
         print("%2d|%s|" % (row_number, "|".join(row)))
         row_number += 1
 
+# Functions for placement of ships and board initial setup
 
-#Functions for placement of ships and board initial setup
 
 def can_place_ship(board, row, col, ship_size, direction):
     if direction == 'horizontal':
@@ -115,6 +110,7 @@ def can_place_ship(board, row, col, ship_size, direction):
                 return False
     return True
 
+
 def place_ship(board, row, col, ship_size, direction):
     if direction == 'horizontal':
         for i in range(ship_size):
@@ -122,6 +118,7 @@ def place_ship(board, row, col, ship_size, direction):
     else:  # direction == 'vertical'
         for i in range(ship_size):
             board[row + i][col] = 'X'
+
 
 def create_ships(board, board_size, ship_sizes):
     for ship_size in ship_sizes:
@@ -135,10 +132,12 @@ def create_ships(board, board_size, ship_sizes):
                 placed = True
 
 
-#Functions for game play
-#-----------------------
+# Functions for game play
+# -----------------------
 
-#Function to selct a target
+# Function to selct a target
+
+
 def get_target_location(board_size):
     row = input(f'Enter a row number to target (1-{board_size}): ')
     while not row.isdigit() or not 1 <= int(row) <= board_size:
@@ -146,16 +145,26 @@ def get_target_location(board_size):
         print("Enter a valid row number")
         print("------------------------")
         row = input(f'Enter a row number to target (1-{board_size}): ')
-    column = input(f'Enter a column letter to target (A-{chr(ord("A") + board_size - 1)}): ').upper()
-    while len(column) != 1 or column < 'A' or column > chr(ord("A") + board_size - 1):
+    column = input(
+        f'Enter column letter (A-{chr(ord("A") + board_size - 1)}): '
+        ).upper()
+    while (
+        len(column) != 1 or
+        column < 'A' or
+        column > chr(ord("A") + board_size - 1)
+    ):
         print("---------------------------")
         print("Enter a valid column letter")
         print("---------------------------")
-        column = input(f'Enter a column letter to target (A-{chr(ord("A") + board_size - 1)}): ').upper()
+        column = (
+            input(f'Enter target col. (A-{chr(ord("A") + board_size - 1)}): ')
+            .upper()
+        )
     return int(row) - 1, letters_to_numbers[column]
 
 
-#FUnction for sonar 
+# FUnction for sonar
+
 
 def sonar(board, row, col, board_size):
     sonar_range = 1
@@ -171,38 +180,41 @@ def sonar(board, row, col, board_size):
             else:
                 print(f"No ship detected at {r + 1}, {chr(c + 65)}")
 
-#Functions for game progress check-ins
+# Functions for game progress check-ins
+
 
 def sunk_ships(board):
-    count = 0 
+    count = 0
     for row in board:
         for column in row:
-           if column == 'X':
-            count += 1
-    return count 
+            if column == 'X':
+                count += 1
+    return count
+
 
 def check_game_over(red_board, blue_board):
     total_ships = sum(row.count('X') for row in red_board)
     total_hits = sum(row.count('X') for row in blue_board)
     return total_hits == total_ships
 
-#Game play
-#---------
+# Game play
+# ---------
 
-#Setup 
+# Setup
+
 
 board_size = choose_board_size()
 difficulty = choose_difficulty()
 ship_sizes_for_game = ship_sizes[difficulty]
 
-#Create boards
+# Create boards
 RED_BOARD = [[' '] * board_size for _ in range(board_size)]
 BLUE_BOARD = [[' '] * board_size for _ in range(board_size)]
 
-# Set ships on opponent board 
+# Set ships on opponent board
 create_ships(RED_BOARD, board_size, ship_sizes_for_game)
 
-#Game Loop
+# Game Loop
 print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
 print("Would you like to read the game rules? (Y/N)")
 read_rules = input().upper()
@@ -211,10 +223,10 @@ if read_rules == 'Y':
 elif read_rules == 'N':
     pass
 else:
-    print("-----------------------------") 
+    print("-----------------------------")
     print("Invalid input. Starting game.")
     print("-----------------------------")
-turns = 30 
+turns = 30
 while turns > 0:
     print("^^^^^^^^^^^^^^^^^^^^^^^")
     print('Prepare for Battleships')
@@ -227,19 +239,20 @@ while turns > 0:
         print("---------------")
     elif RED_BOARD[row][column] == 'X':
         print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
-        print ('Congratulations You hit a battleship')
+        print('Congratulations You hit a battleship')
         print("---------------")
         print("---------------")
         BLUE_BOARD[row][column] = 'X'
         sonar(RED_BOARD, row, column, board_size)
         turns -= 1
         if check_game_over(RED_BOARD, BLUE_BOARD):
-            print("Congratulations! You've sunk all of your opponent's battleships!")
+            print("Congratulations! You sank all of the battleships!")
             print("Thankyou for playing. Goodbye.")
-            print('If you would like to play again click the "Run Program" button at the top of the page.')
+            print('If you would like to play again click')
+            print('the "Run Program" button at the top of the page.')
             break
     else:
-        print("^^^^^^^^^^^^^^^") 
+        print("^^^^^^^^^^^^^^^")
         print("You missed! :(")
         print("---------------")
         print("---------------")
@@ -249,8 +262,8 @@ while turns > 0:
     print("You have " + str(turns) + " turns remaining")
     if turns <= 0:
         print('You Have No More Turns - Game Over')
-        print('Thank you for playing battleships. Better Luck Next Time. Gooodbye.')
-        print('If you would like to play again click the "Run Program" button at the top of the page.')
+        print('Thank you for playing battleships.')
+        print('Better Luck Next Time. Gooodbye.')
+        print('If you would like to play again click')
+        print('the "Run Program" button at the top of the page.')
         break
-
-
